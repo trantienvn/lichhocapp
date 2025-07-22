900ca137f635c3cc2e16a61c6987f5f9b5c522e8
Tran Tien <tientn2005@gmail.com>
2025-07-22 19:48:27 +0700

    tu dong tao ten mien

--- web.bat
+++ web.bat
@@ -12,5 +12,6 @@ IF ERRORLEVEL 1 (
 
 echo Di chuyen build\web sang docs...
 move build\web docs
-
+echo lichhoc.trantien.id.vn > docs\CNAME
+echo Da tao ten mien.
 echo Hoan tat. Web nam trong thu muc docs\
